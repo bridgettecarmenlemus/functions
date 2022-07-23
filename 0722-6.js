417b1744c96036ba52e58be2dc8e1d5b7c61612b
Bridgette Lemus <bridgettelemus@Bridgettes-MacBook-Pro.local>
2022-07-22 23:19:16 -0400

    function that takes an array and returns 1st element

---
+++ 0722-6.js
@@ -0,0 +1,11 @@
+// Create a function that takes an array containing only numbers and return the first element.
+
+
+
+let array = [1,2,3,4]
+
+function getFirstValue([]){
+    return array.shift()
+}
+
+console.log(getFirstValue([]))
