5eda26d4cab8700b2ebe79315f84805f5d5d034a
Bridgette Lemus <bridgettelemus@Bridgettes-MacBook-Pro.local>
2022-07-22 22:54:12 -0400

    function increments number by 1

---
+++ 0722-5.js
@@ -0,0 +1,7 @@
+// Create a function that takes a number as an argument, increments the number by +1 and returns the result.
+
+function addSum (num){
+    return (num)+1
+}
+
+console.log(addSum(8))
