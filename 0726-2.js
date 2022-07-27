5f90b06d82216e995aa813fd7a36dd2a0d48876b
Bridgette Lemus <bridgettelemus@Bridgettes-MacBook-Pro.local>
2022-07-26 21:34:26 -0400

    function that returns perimeter of circle or square

---
+++ 0726-2.js
@@ -0,0 +1,17 @@
+// Write a function that takes a number and returns the perimeter of either a circle or a square. The input will be in the form (letter l, number num) where the letter will be either "s" for square, or "c" for circle, and the number will be the side of the square or the radius of the circle.
+
+// function perimeter(l, num){
+//     let l = 4
+//     letter * num
+
+// }
+
+// console.log(perimeter("s",7))
+
+// my code above and solution found on edabit below
+
+function perimeter(l,num){
+	return (4*(l=='s')+6.28*(l=='c'))*num
+}
+
+console.log(perimeter('s', 4))
